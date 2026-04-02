40c990c8af6a912d1250f8935bfb630cf48febac
youssef werrad <youssefwerrad@gmail.com>
2026-04-02 11:03:01 +0100

    Updates

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: '/portfolio_website/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

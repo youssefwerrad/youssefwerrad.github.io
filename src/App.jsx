48984e50dd0c5e23f121fdc955744f2842d6a901
youssef werrad <youssefwerrad@gmail.com>
2026-04-02 11:26:50 +0100

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,10 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter
+        basename="/portfolio_website"
+        future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+      >
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
